f093bfc91879fe25ed5c0e6d07ff2b579588f764
facuBotta <nutuestampados@gmail.com>
2024-05-02 15:11:28 +0200

    tableau de cartes faite, getCard() marche bien

---
+++ public/js/carts.js
@@ -0,0 +1,362 @@
+
+// treffle-clubs / pique-spades / coeur-hearts / carreau-diamonts 
+
+const cards = [
+    // piques noirs
+    {
+        image: '../images/cartes/card_spades_02.png',
+        value: 2,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        image: '../images/cartes/card_spades_03.png',
+        value: 3,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        image: '../images/cartes/card_spades_04.png',
+        value: 4,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        image: '../images/cartes/card_spades_05.png',
+        value: 5,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        image: '../images/cartes/card_spades_06.png',
+        value: 6,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        image: '../images/cartes/card_spades_07.png',
+        value: 7,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        image: '../images/cartes/card_spades_08.png',
+        value: 8,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        image: '../images/cartes/card_spades_09.png',
+        value: 9,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        image: '../images/cartes/card_spades_10.png',
+        value: 10,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        // J
+        image: '../images/cartes/card_spades_J.png',
+        value: 10,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        // Q
+        image: '../images/cartes/card_spades_Q.png',
+        value: 10,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        // K
+        image: '../images/cartes/card_spades_K.png',
+        value: 10,
+        color: 'noir',
+        categorie: 'pique'
+    },
+    {
+        // A
+        image: '../images/cartes/card_spades_A.png',
+        value: [1, 10],
+        color: 'noir',
+        categorie: 'pique'
+    },
+    // careau roujes
+    {
+        image: '../images/cartes/card_diamonds_02.png',
+        value: 2,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        image: '../images/cartes/card_diamonds_03.png',
+        value: 3,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        image: '../images/cartes/card_diamonds_04.png',
+        value: 4,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        image: '../images/cartes/card_diamonds_05.png',
+        value: 5,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        image: '../images/cartes/card_diamonds_06.png',
+        value: 6,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        image: '../images/cartes/card_diamonds_07.png',
+        value: 7,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        image: '../images/cartes/card_diamonds_08.png',
+        value: 8,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        image: '../images/cartes/card_diamonds_09.png',
+        value: 9,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        image: '../images/cartes/card_diamonds_10.png',
+        value: 10,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        // J
+        image: '../images/cartes/card_diamonds_J.png',
+        value: 10,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        // Q
+        image: '../images/cartes/card_diamonds_Q.png',
+        value: 10,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        // K
+        image: '../images/cartes/card_diamonds_K.png',
+        value: 10,
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    {
+        // A
+        image: '../images/cartes/card_diamonds_A.png',
+        value: [1, 10],
+        color: 'rouge',
+        categorie: 'careau'
+    },
+    // Treffle
+    {
+        image: '../images/cartes/card_clubs_02.png',
+        value: 2,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        image: '../images/cartes/card_clubs_03.png',
+        value: 3,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        image: '../images/cartes/card_clubs_04.png',
+        value: 4,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        image: '../images/cartes/card_clubs_05.png',
+        value: 5,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        image: '../images/cartes/card_clubs_06.png',
+        value: 6,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        image: '../images/cartes/card_clubs_07.png',
+        value: 7,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        image: '../images/cartes/card_clubs_08.png',
+        value: 8,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        image: '../images/cartes/card_clubs_09.png',
+        value: 9,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        image: '../images/cartes/card_clubs_10.png',
+        value: 10,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        // J
+        image: '../images/cartes/card_clubs_J.png',
+        value: 10,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        // Q
+        image: '../images/cartes/card_clubs_Q.png',
+        value: 10,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        // K
+        image: '../images/cartes/card_clubs_K.png',
+        value: 10,
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    {
+        // A
+        image: '../images/cartes/card_clubs_A.png',
+        value: [1, 10],
+        color: 'noir',
+        categorie: 'treffle'
+    },
+    // Coeurs
+    {
+        image: '../images/cartes/card_hearts_02.png',
+        value: 2,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        image: '../images/cartes/card_hearts_03.png',
+        value: 3,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        image: '../images/cartes/card_hearts_04.png',
+        value: 4,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        image: '../images/cartes/card_hearts_05.png',
+        value: 5,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        image: '../images/cartes/card_hearts_06.png',
+        value: 6,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        image: '../images/cartes/card_hearts_07.png',
+        value: 7,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        image: '../images/cartes/card_hearts_08.png',
+        value: 8,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        image: '../images/cartes/card_hearts_09.png',
+        value: 9,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        image: '../images/cartes/card_hearts_10.png',
+        value: 10,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        // J
+        image: '../images/cartes/card_hearts_J.png',
+        value: 10,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        // Q
+        image: '../images/cartes/card_hearts_Q.png',
+        value: 10,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        // K
+        image: '../images/cartes/card_hearts_K.png',
+        value: 10,
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+    {
+        // A
+        image: '../images/cartes/card_hearts_A.png',
+        value: [1, 10],
+        color: 'rouge',
+        categorie: 'coeur'
+    },
+]
+let cardsInGame = cards;
+let cardsUsed = [];
+
+
+function getCard(tab) {
+    // selectioner une carte
+    const randIndex = Math.floor(Math.random() * (tab.length));
+    const cardSelected = tab[randIndex];
+
+    // Ajouter la carte au tableau de cartes utilises
+    cardsUsed.push(cards[randIndex]);
+
+    // actualiser le tableau de cartes qui restent
+    const tempCards = cardsInGame.filter(card => card.image != cardSelected.image);
+    cardsInGame = tempCards;
+
+    return cardSelected;
+}
+
+
+getCard(cardsInGame);
+
+console.log('original :    ' + cards.length)
+console.log('in game :    ' + cardsInGame.length)
+console.log('trash :   ' + cardsUsed.length)
