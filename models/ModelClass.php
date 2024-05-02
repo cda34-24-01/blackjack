7e186cfba8f8427fe0bd0830c77448e649d89380
NAME <EMAIL>
2024-05-02 11:10:39 +0200

    add files

---
+++ models/ModelClass.php
@@ -0,0 +1,20 @@
+<?php
+abstract class Model
+{
+    private $pdo;
+
+    private function setDB()
+    {
+        $this->pdo = new PDO("mysql:host=localhost;dbname=blackjack;port=3306;charset=utf8", 'root', '123456');
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    protected function getDB()
+    {
+        if ($this->pdo == null) {
+            $this->setDb();
+        }
+        return $this->pdo;
+    }
+}
+
