b4b44f3e1be271f112a1d20b1bbf4df37f75eb23
NAME <EMAIL>
2024-05-03 11:26:07 +0200

    chore: Refactor table_view.php to improve code readability and maintainability

--- views/table_view.php
+++ views/table_view.php
@@ -1,26 +1,34 @@
+<?php 
+require_once "models/manager/UserManager.php";
+?>
+
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blackjack</title>
+    <link rel="stylesheet" href="<?= URL . "public/css/general.css" ?>">
     <link rel="stylesheet" href="<?= URL . "public/css/table.css" ?>">
 
 </head>
+
 <body>
-<header>
-    <div class="logo">BlackJack</div>
-    <div class="user">Marceau</div>
-</header>
+    <header>
+        <div class="logo">BlackJack</div>
+        <div class="user"><?= $user['pseudo'] ?></div>
+    </header>
     <div class="table">
         <div class="ia_card_section">
             <div class="ia_card">
                 <!-- <div class="ia_card_1 cards_container "></div> -->
                 <!-- <div class="ia_card_2 cards_container "></div> -->
             </div>
-            <div class="ia_number"><p>8</p></div>
+            <div class="ia_number">
+                <p>8</p>
+            </div>
         </div>
 
         <div class="user_card_section">
@@ -33,19 +41,40 @@
                 <p></p>
             </div>
             <div class="btn_section">
-                <button id="hit" >Action1</button>
-                <button id="act2" >action2</button>
-                <button id="act3" >action3</button>
+                <button id="act1">Action1</button>
+                <button id="act2">action2</button>
+                <button id="act3">action3</button>
             </div>
         </div>
 
+            <button class="btn" onclick="<?= $this->userManager->removeMoney($user['id_utilisateur'], 1) ?>">
+                +1
+            </button>
+            <button class="btn">
+                +5
+            </button>
+            <button class="btn">
+                +25
+            </button>
+            <button class="btn">
+                +50
+            </button>
+            <button class="btn">
+                +100
+            </button>
+            <button class="btn">
+                +500
+            </button>
+            <button class="btn">
+                +1000
+            </button>
         <div class="player_ui">
-            <div class="ui_money">Money : 1000</div>
-            <div class="ui_wins">Wins = 10</div>
-            <div class="ui_loses">Loses = 5</div>
+            <div class="ui_money">Argent : <?= $user['money'] ?></div>
+            <div class="ui_wins">Victoires : <?= $user['wins'] ?></div>
+            <div class="ui_loses">Défaites : <?= $user['loses'] ?></div>
         </div>
     </div>
-
-<script type="module" src="<?= URL . "public/js/general.js" ?> "></script>
+    <script type="module" src="<?= URL . "public/js/general.js" ?> "></script>
 </body>
+
 </html>
