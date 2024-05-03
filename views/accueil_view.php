8102d71b244cec2da0ce899427c8173092745ee7
hlmekki <hamdani.lmekki@gmail.com>
2024-05-03 14:24:27 +0200

    applying many style to home page V1

--- views/accueil_view.php
+++ views/accueil_view.php
@@ -14,6 +14,10 @@ $_SESSION['previous_url'] = $_SERVER['REQUEST_URI']; */
     <meta name="description" content="Test BlackJack">
 
     <link href="<?= URL . "public/css/general.css" ?>" rel="stylesheet" type="text/css">
+    <link href="<?= URL . "public/css/accueil.css" ?>" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Sedan+SC&display=swap" rel="stylesheet">
 
     <link rel="icon" type="image/svg+xml" href="<?= URL . "public/images/cineramaimg.svg" ?>">
 </head>
@@ -24,47 +28,35 @@ $_SESSION['previous_url'] = $_SERVER['REQUEST_URI']; */
         /* include("navbar.php"); */
         ?>
     </header>
+
     <main>
-        <div>
-            <h1>BlackJack</h1>
-            <p>Le BlackJack est un jeu de cartes très populaire dans les casinos. Le but du jeu est de battre le croupier en obtenant un total de points supérieur à celui-ci sans dépasser 21. Le jeu se joue avec un ou plusieurs jeux de 52 cartes. Chaque carte a une valeur qui lui est propre. Les cartes numérotées de 2 à 10 valent leur valeur faciale. Les figures (valet, dame et roi) valent 10 points. L'as vaut 1 ou 11 points, au choix du joueur. Le croupier distribue deux cartes à chaque joueur et une à lui-même. Les joueurs peuvent alors demander des cartes supplémentaires (« tirer une carte ») pour se rapprocher de 21. Si le total des points des cartes d'un joueur dépasse 21, il perd la partie. Le croupier doit tirer des cartes supplémentaires jusqu'à ce qu'il atteigne un total de 17 ou plus. Si le croupier dépasse 21, tous les joueurs encore en jeu gagnent. Si le croupier ne dépasse pas 21, les joueurs ayant un total de points supérieur au sien gagnent. Si le croupier et un joueur ont le même total de points, il y a égalité.</p>
-        </div>
-        <div>
-            <h2>Comment jouer ?</h2>
-            <p>Le joueur doit choisir la mise qu'il souhaite placer en cliquant sur les jetons. Il peut ensuite cliquer sur le bouton "Distribuer" pour recevoir ses deux premières cartes. Le joueur peut alors choisir de "Rester" ou de "Tirer une carte". S'il choisit de tirer une carte, il peut continuer à en tirer jusqu'à ce qu'il décide de rester ou qu'il dépasse 21. Le croupier tire ensuite des cartes jusqu'à ce qu'il atteigne un total de 17 ou plus. Le joueur gagne si son total de points est supérieur à celui du croupier sans dépasser 21. Le joueur peut également gagner en obtenant un Blackjack
-            </p>
-        </div>
-        <div>
-            <h2>Les règles du Blackjack</h2>
-            <p>Le Blackjack est un jeu de cartes très populaire dans les casinos. Le but du jeu est de battre le croupier en obtenant un total de points supérieur à celui-ci sans dépasser 21. Le jeu se joue avec un ou plusieurs jeux de 52 cartes. Chaque carte a une valeur qui lui est propre. Les cartes numérotées de 2 à 10 valent leur valeur faciale. Les figures (valet, dame et roi) valent 10 points. L'as vaut 1 ou 11 points, au choix du joueur. Le croupier distribue deux cartes à chaque joueur et une à lui-même. Les joueurs peuvent alors demander des cartes supplémentaires (« tirer une carte ») pour se rapprocher de 21. Si le total des points des cartes d'un joueur dépasse 21, il perd la partie. Le croupier doit tirer des cartes supplémentaires jusqu'à ce qu'il atteigne un total de 17 ou plus. Si le croupier dépasse 21, tous les joueurs encore en jeu gagnent. Si le croupier ne dépasse pas 21, les joueurs ayant un total de points supérieur au sien gagnent. Si le croupier et un joueur ont le même total de points, il y a égalité.</p>
-        </div>
-        <div>
-            <h2>Les combinaisons gagnantes</h2>
-            <p>Le joueur gagne s'il obtient un total de points supérieur à celui du croupier sans dépasser 21. Le joueur peut également gagner en obtenant un Blackjack, c'est-à-dire un total de 21 points avec ses deux premières cartes. Le Blackjack est composé d'un as et d'une carte valant 10 points. Le joueur gagne alors 1,5 fois sa mise. Si le croupier obtient un Blackjack, tous les joueurs ayant un total de points inférieur à 21 perdent leur mise. Si le joueur et le croupier ont tous les deux un Blackjack, il y a égalité et le joueur récupère sa mise.</p>
-        </div>
-        <div>
-            <h2>Les combinaisons perdantes</h2>
-            <p>Le joueur perd s'il dépasse 21 points. Il perd également s'il obtient un total de points inférieur à celui du croupier sans dépasser 21. Si le croupier obtient un total de points supérieur à 21, tous les joueurs encore en jeu gagnent. Si le joueur et le croupier ont le même total de points, il y a égalité et le joueur récupère sa mise.</p>
-        </div>
+        
+    <div class="title">
+<h1>Black Jack</h1>
 
-
-        <?php if (isset($_SESSION['id'])) { ?>
-            <div class="btn">
-                <button class="fs-5 bgVertFonce"><a href="table" class="blanc">PLAY</a></button>
+</div>
+<div class="button_main">
+<?php if (isset($_SESSION['id'])) { ?>
+            <div >
+                <button id="play_btn" class="log_btn"><a href="table" >PLAY</a></button>
             </div>
         <?php } else { ?>
-            <div class="btn">
-                <button class="fs-5 bgVertFonce"><a href="connexion" class="blanc">CONNEXION</a></button>
+            <div >
+                <button class="log_btn"><a href="connexion" >CONNEXION</a></button>
             </div>
-            <div class="btn">
-                <button class="fs-5 bgJaune"><a href="inscription" class="noir">INSCRIPTION</a></button>
+            <div class="">
+                <button class="log_btn"><a href="inscription" >INSCRIPTION</a></button>
             </div>
         <?php } ?>
         <?php if (isset($_SESSION['id'])) { ?>
-            <div class="btn">
-                <button class="fs-5 bgRouge"><a href="deconnexion" class="blanc">DECONNEXION</a></button>
+            <div class="">
+                <button class="log_btn"><a href="deconnexion" >DECONNEXION</a></button>
             </div>
         <?php } ?>
+</div>
+
+   
+
 
     </main>
 
@@ -74,7 +66,7 @@ $_SESSION['previous_url'] = $_SERVER['REQUEST_URI']; */
         ?>
     </footer>
 
-    <script src="<?= URL . "public/js/general.js" ?>" type="module"></script>
+    <script src="<?= URL . "public/js/general.js" ?>"></script>
 </body>
 
 </html>
